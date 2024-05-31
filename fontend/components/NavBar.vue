90e1d670c9559abff30de3f9eaaacbf702e7a8bb
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2024-05-31 21:25:00 +0600

    laravel nuxt project setup

---
+++ fontend/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container mx-auto px-4">
+      <div class="p-2 flex justify-between items-center">
+        <NuxtLink to="/" class="text-black text-3xl shadow">Home</NuxtLink>
+      </div>
+    </div>
+</template>
---
+++ fontend/pages/index.vue
@@ -0,0 +1,5 @@
+<template> 
+<div class="p-4 bg-red-500">
+
+</div>
+</template>
